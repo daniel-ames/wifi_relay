0b7e284e756cab8f9ee4b9278df5979ca7be8b06
ames <daniel.ames@hp.com>
2023-08-20 15:55:22 -0500

    Now actually wite the submitted form data to the EEPROM. Also throw an error to the user if they exceed the char limits for ssid and password

--- html_strings.h
+++ html_strings.h
@@ -1,10 +1,28 @@
 
 
-
+char* welcome_form =
+"<div><span style=\"font-size:30px\">I'm connected!</span></div>";
 
 char* config_form = 
-"<div>Connect me to your wifi</div>"
+"<div><span style=\"font-size:30px\">Connect me to your wifi</span></div>"
 "<form action=\"http://192.168.4.1/setConfig?ssid=foo&password=bar\" method=\"GET\">"
+"  <div>"
+"    <label for=\"say\">SSID:</label>"
+"    <input name=\"say\" id=\"say\" value=\"nothing\"/>"
+"  </div>"
+"  <div>"
+"    <label for=\"to\">Password:</label>"
+"    <input name=\"to\" id=\"to\" value=\"mom\"/>"
+"  </div>"
+"  <div>"
+"    <button>Connect</button>"
+"  </div>"
+"</form>";
+
+char* config_with_errors_form = 
+"<div><span style=\"color:Red;font-size:30px\">---ERROR: max length of SSID is 32 characters, and max length of Password is 63 characters---</span></div>"
+"<div><span style=\"font-size:30px\">Connect me to your wifi</span></div>"
+"<form action=\"http://192.168.4.1/setConfig\" method=\"GET\">"
 "  <div>"
 "    <label for=\"say\">SSID:</label>"
 "    <input name=\"say\" id=\"say\" value=\"nothing\"/>"

66a2f711a78ca6a7e8a75920c935d9537de504bc
ames <daniel.ames@hp.com>
2023-08-20 11:25:19 -0500

    Add helper to get strings from eeprom. Bifurcate logic based on whether an ssid is configured or not.

--- wifi_relay.ino
+++ wifi_relay.ino
@@ -9,12 +9,18 @@
 
 
 #define out(...) Serial.printf(__VA_ARGS__)
+#define outl  Serial.println()
 
 
-#define EEPROM_WIFI_CONFIGURED_BYTE  0    // 0 = wifi NOT configured, 1 = wifi configured
-#define EEPROM_SSID                  1    // ssid string, no null
-#define EEPROM_PASSWORD              33   // password string, no null
-#define EEPROM_NUMBER_OF_BYTES_WE_USE  (1 + 32 + 63)  // 1 for the wifi_configured byte, 32 for ssid max length of 32, 63 for password max length of 63. (According to the googles)
+#define EEPROM_WIFI_CONFIGURED_BYTE      0    // 0 = wifi NOT configured, 1 = wifi configured
+#define EEPROM_SSID_OFFSET               1
+#define EEPROM_SSID_LENGTH               32   // max length for ssid, no null (according to the googles)
+#define EEPROM_PASSWORD_OFFSET           33
+#define EEPROM_PASSWORD_LENGTH           63   // max length for password, no null (according to the googles)
+
+#define EEPROM_NUMBER_OF_BYTES_WE_USE    (1 + EEPROM_SSID_LENGTH + EEPROM_PASSWORD_LENGTH)
+
+#define DEFAULT_SSID     "wifi_relay"
 
 
 ESP8266WebServer server(80);
@@ -25,6 +31,28 @@ String get_eeprom_string(int offset, int length) {
     str += EEPROM.read(i);
   }
   return str + '\0';
+}
+
+void handleSetup() {
+  out("Received wifi config data\n");
+
+  String ssid = "";
+  String password = "";
+
+  if (server.args() == 2) {
+    ssid = server.arg(0);
+    password = server.arg(1);
+
+    out("SSID: %s\n", ssid);
+    out("Pass: %s\n", password);
+
+    server.send(200);
+  } else {
+    out("nae data!!\n");
+    server.send(401);
+  }
+
+
 }
 
 
@@ -39,18 +67,66 @@ void setup() {
   char wifi_configured = EEPROM.read(EEPROM_WIFI_CONFIGURED_BYTE);
   //EEPROM.write(EEPROM_WIFI_CONFIGURED_BYTE, (byte)notificationsMuted);
   //EEPROM.commit();
+  out("mystery char: %c\n", wifi_configured);
 
 
   if (wifi_configured) {
-    String ssid = get_eeprom_string();
-    String password = EEPROM.read(10);;
+    String ssid = get_eeprom_string(EEPROM_SSID_OFFSET, EEPROM_SSID_LENGTH);
+    String password = get_eeprom_string(EEPROM_PASSWORD_OFFSET, EEPROM_PASSWORD_LENGTH);
     WiFi.mode(WIFI_STA);
-
+    WiFi.begin(ssid, password);
+    out("Connecting to %s", ssid);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      out(".");
+    }
+    out("connected.\n");
+  } else {
+    out("No ssid configured\n");
+    out("Entering AP mode. SSID: %s\n", DEFAULT_SSID);
+    WiFi.softAP(DEFAULT_SSID);
+    out("APIP: ");
+    Serial.println(WiFi.softAPIP());
+    server.on("/", HTTP_GET, handleSetup);
+    server.begin();
   }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  server.handleClient();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
